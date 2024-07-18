4e176f47a588ead99fa29b610514b1f2a83fc6e3
Wyatt Mattas <mattaswm@gmail.com>
2024-07-18 15:47:50 -0500

    Delete sensorforge-mag/target directory

--- sensorforge-mag/target/debug/deps/sensorforge_mag-9aab64a7634b9f1c.d
+++
@@ -1,5 +0,0 @@
-C:\git_clone\SensorForge\sensorforge-mag\target\debug\deps\libsensorforge_mag-9aab64a7634b9f1c.rmeta: src\lib.rs
-
-C:\git_clone\SensorForge\sensorforge-mag\target\debug\deps\sensorforge_mag-9aab64a7634b9f1c.d: src\lib.rs
-
-src\lib.rs:
--- sensorforge-mag/target/debug/deps/sensorforge_mag-9ad4d20050f3c417.d
+++
@@ -1,5 +0,0 @@
-C:\git_clone\SensorForge\sensorforge-mag\target\debug\deps\libsensorforge_mag-9ad4d20050f3c417.rmeta: src\lib.rs
-
-C:\git_clone\SensorForge\sensorforge-mag\target\debug\deps\sensorforge_mag-9ad4d20050f3c417.d: src\lib.rs
-
-src\lib.rs:
